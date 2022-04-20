c20dff400858b9a6cb823734fda1ebaafcdc61b1
Yorvin Luna Wisky <yorvin0506@gmail.com>
2022-04-20 15:37:35 -0400

    404 error resolved.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -20,45 +20,16 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
     );
     next();
-});
+  });
 
-app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save();
-    res.status(201).json({
-        message: 'Post added successfully!'
-    });
-
-});
-
-
-app.get('/api/posts', (req, res, next) => {
-  
-    Post.find().then(documents => {
-        res.status(200).json({
-            message: 'Post fetched successfully!',
-            posts: documents
-        });
-    });
-    
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({ message: "Post deleted!"});
-    });    
-});
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
